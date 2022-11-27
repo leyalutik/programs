ce843b61159c7c6a7d67a773438766e6632bae6d
leyalutik <ludapevek@gmail.com>
2022-11-27 11:40:17 +0300

    27.11.2022 11:40:17,57

---
+++ git_push.bat
@@ -0,0 +1,5 @@
+git remote set-url origin git@github.com:leyalutik/programs.git
+git add .
+git commit -m " %date% %time%"
+git push -u origin master
+git remote set-url origin git@github.com:leyalutik/leya.git
